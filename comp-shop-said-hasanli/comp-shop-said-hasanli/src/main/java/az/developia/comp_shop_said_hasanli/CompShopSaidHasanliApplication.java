a0d8cb06e7ef1dc1951a3685521263baa877cb24
saidhasanli14 <sajddosz@gmail.com>
2025-01-05 17:19:19 +0400

    comp-shop project created

---
+++ comp-shop-said-hasanli/comp-shop-said-hasanli/src/main/java/az/developia/comp_shop_said_hasanli/CompShopSaidHasanliApplication.java
@@ -0,0 +1,13 @@
+package az.developia.comp_shop_said_hasanli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompShopSaidHasanliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompShopSaidHasanliApplication.class, args);
+	}
+
+}
---
+++ comp-shop-said-hasanli/comp-shop-said-hasanli/src/test/java/az/developia/comp_shop_said_hasanli/CompShopSaidHasanliApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.comp_shop_said_hasanli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompShopSaidHasanliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
